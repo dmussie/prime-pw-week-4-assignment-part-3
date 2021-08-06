2b4fc58d7f7e77ba7f2e12fe6536be319dfce549
Danny Mussie <dannymussie7@gmail.com>
2021-08-06 08:30:20 -0500

    completed week 4 assignment pt 3

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,22 +1,36 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
-let basket = [];
-function addItem ( item ) {
-  basket += item;
-  return true;
-};
+
 
 // addItem function
-
-function listItems () {
-  for (item in basket) {
-    console.log(item);
-  }
+let basket = [];
+function addItem ( item ) {
+  basket.push(item);
+  return true;
 };
-// listItems function
+console.log(`Basket is empty: ${basket}`);
+console.log('Adding apples (expect true)', addItem('apples'));
+console.log('Adding oranges (expect true)', addItem('oranges'));
+console.log('Adding strawberries (expect true)', addItem('strawberries'));
+// console.log('Adding carrots (expect true)', addItem('carrots'));
+// console.log('Adding salad (expect true)', addItem('salad'));
+console.log(`Basket is now ${basket}`);
 
-function empty () {
+
+// listItems function
+function listItems () {
+  for (let i=0; i < basket.length; i++) {
+    console.log(basket[i]);
+  }
   return basket;
 };
+console.log('Basket items listed in their own row', listItems());
+
+
 // empty function
+function empty () {
+  let basket = [];
+  return basket;
+};
+console.log('Resetting back to an empty array:', empty());
